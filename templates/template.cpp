78d617e619d6162ac4f3331a10a42267161a3be8
rve <jiongrr@gmail.com>
2013-04-19 16:24:17 +0800

    update templates

--- templates/template.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include<string>
-#include<cstring>
-#include<algorithm>
-#include<cstdio>
-#define MAXN 10010
-#define INF 0x3f3f3f3f
-using namespace std;
---
+++ templates/template_minimal.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<map>
+#include<queue>
+#include<set>
+#include<vector>
+#include<cstring>
+#include<algorithm>
+#include<cstdio>
+#define MAXN 10010
+#define INF 0x5f3759df //for magic
+using namespace std;
+const double  PI = acos(-1.0); //M_PI;
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
