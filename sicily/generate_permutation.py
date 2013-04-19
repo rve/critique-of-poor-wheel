a884f96b9263eeb7e7d858edab1c755a58850ceb
rve <jiongrr@gmail.com>
2013-04-19 16:26:58 +0800

    miscs

--- sicily/generate_permutation.py
+++
@@ -1,9 +0,0 @@
-from collections import deque
-
-g = deque([1, 2, 3, 4])
-
-for i in range(len(g)):
-    #print g #or do anything with permutation
-    g.rotate(1) #for right rotation
-    #or g.rotate(-1) for left rotation
-
