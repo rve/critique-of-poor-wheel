e8ddbaa6edc5f8cb158feed8c3083fd16a940645
rve <jiongrr@gmail.com>
2013-04-21 00:21:52 +0800

    update templ

--- templates/template_minimal.cpp
+++ templates/template_minimal.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<utility>
 #include<string>
 #include<cmath>
 #include<map>
@@ -10,7 +11,22 @@
 #include<cstdio>
 #define MAXN 10010
 #define INF 0x5f3759df //for magic
+
+//#define DEBUG
+#ifdef DEBUG
+#define debug(...) printf( __VA_ARGS__) 
+#else
+#define debug(...)
+#endif
+
 using namespace std;
 const double  PI = acos(-1.0); //M_PI;
 const int dx[] = {-1, 0, 1, 0};
 const int dy[] = {0, 1, 0, -1};
+
+int main()
+{
+#ifdef DEBUG  
+    freopen("data.in","r",stdin);  
+#endif 
+}
