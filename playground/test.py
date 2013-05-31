ee509b785720a5706f82f0a68554bd66871b838c
rve <jiongrr@gmail.com>
2013-05-31 13:04:00 +0800

    minor update

--- playground/test.py
+++ playground/test.py
@@ -1,17 +1,15 @@
-def repeat(s, ex) :
-    s = s * 3
-    if ex:
-        s += "!!!"
-    return s
-if __name__ == "__main__" :
-    print repeat("Yooo", True)
-    print repeat("Yummy", False)
-    s = 'hi'
-    print s[1]
-    print s
-    pi = 3.1415
-    print "pi: " + str(pi)
-    list = []
-    list.append('a')
-    list.append('b')
-    print list
+#!/usr/bin/env python2.7
+import os
+def main() :
+    elf1 = "./camelot_try3.o <input"
+    elf11 = ".txt"
+    elf2 = "cat ./ans/output"
+    elf22 = ".txt"
+    for i in range(19):
+        print "Run #%d" % (i+1)
+        os.system(elf1 + str(i+1) + elf11)
+        os.system(elf2 + str(i+1) + elf22)
+
+if __name__ == "__main__":
+    
+    main()
