81e758e95f7bb5e5aacddd71d53a22b8beea766b
rve <jiongrr@gmail.com>
2013-06-11 21:32:55 +0800

    add pi.hs

---
+++ scripts/pi.hs
@@ -0,0 +1,13 @@
+arccot x unity =
+    arccot' x unity 0 start 1 1
+      where start = unity `div` x
+            arccot' x unity sum xpower n sign | xpower `div` n == 0 = sum
+                                              | otherwise           =
+                arccot' x unity (sum + sign*term) (xpower `div` (x*x)) (n+2) (-sign)
+                  where term = xpower `div` n
+
+pi digits = pi' `div` (10 ^ 10)
+                     where unity = 10 ^ (digits+10)
+                           pi' = 4 * (4 * arccot 5 unity - arccot 239 unity)
+
+--try pi 100
